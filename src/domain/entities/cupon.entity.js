871ad391b3554b223b6dd4c3a56055e80a59a215
LeoGMR20Dev <leoguimorales@gmail.com>
2025-12-09 23:02:48 -0400

    feat(cupon): add cupon entity, repository, and controller
    - Created CuponEntity class to represent cupon data.
    - Added cupon.repository.interface.js for repository interface.
    - Implemented cupon.mongo.repository.js for MongoDB interactions.
    - Defined cupon.model.js schema for MongoDB using Mongoose.
    - Added cupon.controller.js for handling cupon-related requests.
    - Introduced cupon.routes.js for routing cupon endpoints.
    - Updated package.json to include jest and supertest for testing.

---
+++ src/domain/entities/cupon.entity.js
@@ -0,0 +1,9 @@
+class CuponEntity {
+  constructor(id, name, amount, isPercentage, minimumPriceToApply) {
+    this.id = id;
+    this.name = name;
+    this.amount = amount;
+    this.isPercentage = isPercentage;
+    this.minimumPriceToApply = minimumPriceToApply;
+  }
+}
